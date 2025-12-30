0c4aca637f82ba0b47da0dbb5e89b75ae01aa939
obed ampah <obed.ampah@ecstech.com>
2025-12-30 08:10:55 -0800

    test - redaction step

--- scubagoggles/reporter/scripts/redaction.js
+++ scubagoggles/reporter/scripts/redaction.js
@@ -1,5 +1,5 @@
 function redaction() {
-    console.log('Starting redaction')
+    console.log('Starting redaction process')
 
     try {
         const identityTable = document.querySelectorAll("table")[0]
