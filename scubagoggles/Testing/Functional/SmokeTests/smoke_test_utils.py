c94c44c4c96c262d57b643e4aa554562d4804f51
obed ampah <obed.ampah@ecstech.com>
2026-01-02 07:37:31 -0800

    test - redaction in selenium execution frankenstein

--- scubagoggles/Testing/Functional/SmokeTests/smoke_test_utils.py
+++ scubagoggles/Testing/Functional/SmokeTests/smoke_test_utils.py
@@ -219,36 +219,36 @@ def run_selenium(browser, customerdomain):
 
     print(dir(browser))
 
-    js_script = """
-            function redaction() {
-                console.log('Starting redaction')
+    # js_script = """
+    #         function redaction() {
+    #             console.log('Starting redaction')
 
-                try {
-                    const identityTable = document.querySelectorAll("table")[0]
+    #             try {
+    #                 const identityTable = document.querySelectorAll("table")[0]
 
-                    let tbody = identityTable.querySelector("tbody")
+    #                 let tbody = identityTable.querySelector("tbody")
 
-                    if (!tbody) throw new Error(
-                        `Invalid HTML structure, <table id='${identityTable.getAttribute("id")}'> does not have a <tbody> tag.`
-                    )
+    #                 if (!tbody) throw new Error(
+    #                     `Invalid HTML structure, <table id='${identityTable.getAttribute("id")}'> does not have a <tbody> tag.`
+    #                 )
 
-                    if (tbody.children[1].children) {
-                        console.log('found identification row: ', tbody.children[1].children)
-                        let identityData = tbody.children[1].children
+    #                 if (tbody.children[1].children) {
+    #                     console.log('found identification row: ', tbody.children[1].children)
+    #                     let identityData = tbody.children[1].children
 
-                        for (let cell of identityData) {
-                            cell.textContent = '[Redacted]'
-                        }
+    #                     for (let cell of identityData) {
+    #                         cell.textContent = '[Redacted]'
+    #                     }
 
-                    }
+    #                 }
 
-                } catch (error) {
-                    console.error(`Error redacting `)
-                }
-            }
-            """
-    browser.execute_script(js_script)
-    browser.execute_script("redaction()")
+    #             } catch (error) {
+    #                 console.error(`Error redacting `)
+    #             }
+    #         }
+    #         """
+    # browser.execute_script(js_script)
+    # browser.execute_script("redaction()")
 
 
     if len(reports_table) == 11:
