cb080929fc00359f359c8cc875beca98b77edaca
obed ampah <obed.ampah@ecstech.com>
2026-01-04 11:33:07 -0800

    test - --disable-full-page-screenshot try

--- scubagoggles/Testing/Functional/SmokeTests/smoke_test_utils.py
+++ scubagoggles/Testing/Functional/SmokeTests/smoke_test_utils.py
@@ -217,34 +217,34 @@ def run_selenium(browser, customerdomain):
     # Before entering loop check that we actually display 9 rows in table
     reports_table = get_reports_table(browser)
 
-    js_script = """
-        console.log('Starting redaction')
+    # js_script = """
+    #     console.log('Starting redaction')
 
-        try {
-            const identityTable = document.querySelectorAll("table")[0]
+    #     try {
+    #         const identityTable = document.querySelectorAll("table")[0]
 
-            let tbody = identityTable.querySelector("tbody")
+    #         let tbody = identityTable.querySelector("tbody")
 
-            if (!tbody) throw new Error(
-                `Invalid HTML structure, <table id='${identityTable.getAttribute("id")}'> does not have a <tbody> tag.`
-            )
+    #         if (!tbody) throw new Error(
+    #             `Invalid HTML structure, <table id='${identityTable.getAttribute("id")}'> does not have a <tbody> tag.`
+    #         )
 
-            if (tbody.children[1].children) {
-                console.log('found identification row: ', tbody.children[1].children)
-                let identityData = tbody.children[1].children
+    #         if (tbody.children[1].children) {
+    #             console.log('found identification row: ', tbody.children[1].children)
+    #             let identityData = tbody.children[1].children
 
-                for (let cell of identityData) {
-                    // cell.textContent = '[Redacted]'
-                    cell.classList.add('.spoiler-text')
-                }
+    #             for (let cell of identityData) {
+    #                 // cell.textContent = '[Redacted]'
+    #                 cell.classList.add('.spoiler-text')
+    #             }
 
-            }
+    #         }
 
-        } catch (error) {
-            console.error(`Error redacting `)
-        }
-            """
-    browser.execute_script(js_script)
+    #     } catch (error) {
+    #         console.error(`Error redacting `)
+    #     }
+    #         """
+    # browser.execute_script(js_script)
 
     if len(reports_table) == 11:
         for i in range(len(reports_table)):
