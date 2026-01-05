a6e94245ad05510210ccbdf8f666aa1d74a16823
obed ampah <obed.ampah@ecstech.com>
2026-01-05 06:46:05 -0800

    test - redaction from the jump

--- scubagoggles/reporter/scripts/main.js
+++ scubagoggles/reporter/scripts/main.js
@@ -1,4 +1,3 @@
-import { redaction } from "./redaction";
 /**
  * Adds the red, green, yellow, and gray coloring to the individual report pages.
  */
@@ -305,6 +304,35 @@ const applyScopeAttributes = () => {
 
     } catch (error) {
         console.error(`Error applying scope attributes: ${error}`)
+    }
+}
+
+// Redaction
+function redaction() {
+    console.log('Starting redaction')
+
+    try {
+        const identityTable = document.querySelectorAll("table")[0]
+
+        let tbody = identityTable.querySelector("tbody")
+
+        if (!tbody) throw new Error(
+            `Invalid HTML structure, <table id='${identityTable.getAttribute("id")}'> does not have a <tbody> tag.`
+        )
+
+        if (tbody.children[1].children) {
+            console.log('found identification row: ', tbody.children[1].children)
+            let identityData = tbody.children[1].children
+
+            for (let cell of identityData) {
+                // cell.textContent = '[Redacted]'
+                cell.classList.add('spoiler-text')
+            }
+
+        }
+
+    } catch (error) {
+        console.error(`Error redacting `)
     }
 }
 
