267ff4c152cd3cdce7d168755cec67639a4228a2
obed ampah <obed.ampah@ecstech.com>
2026-01-06 10:37:24 -0800

    test - updated node version for EOL

--- scubagoggles/Testing/Functional/SmokeTests/smoke_test_utils.py
+++ scubagoggles/Testing/Functional/SmokeTests/smoke_test_utils.py
@@ -382,36 +382,3 @@ def verify_tenant_table(browser, customerdomain, parent):
             error_occurred = True
 
     assert not error_occurred
-
-def redact_info(browser):
-    try:
-        js_script = """
-        console.log('Starting redaction')
-
-        try {
-            const identityTable = document.querySelectorAll("table")[0]
-
-            let tbody = identityTable.querySelector("tbody")
-
-            if (!tbody) throw new Error(
-                `Invalid HTML structure, <table id='${identityTable.getAttribute("id")}'> does not have a <tbody> tag.`
-            )
-
-            if (tbody.children[1].children) {
-                console.log('found identification row: ', tbody.children[1].children)
-                let identityData = tbody.children[1].children
-
-                for (let cell of identityData) {
-                    cell.textContent = '[Redacted]'
-                    // cell.classList.add('.spoiler-text')
-                }
-
-            }
-
-        } catch (error) {
-            console.error(`Error redacting `)
-        }
-            """
-        browser.execute_script(js_script)
-    except:
-        print('redaction failed')
